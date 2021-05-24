1cf41751cfd97c3d676b556a757a47cd52c1ed3a
Ofremu199x <Ofremu199x@gmail.com>
2021-05-24 07:09:34 +0300

    i deleted madlib.py

--- madlib.py
+++
@@ -1,7 +0,0 @@
-hobby1 = input('first hobby?:')
-hobby2 = input('secound hobby?:')
-season = input('favourite season?:')
-
-madlib =(f'I want to go for {hobby1} and play {hobby2} during {season}')
-
-print (madlib)
